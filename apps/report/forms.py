6c1c4e64a17b56bcabbfe7e84a366a1fe81e2616
Mike Garabedian <mike.garabedian@riverbed.com>
2013-03-18 09:19:56 -0400

    Include missing copyright header

--- apps/report/forms.py
+++ apps/report/forms.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").
+# This software is distributed "AS IS" as set forth in the License.
+
 from django import forms
 
 from apps.report.models import Report, Widget
