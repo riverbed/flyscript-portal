0acf5e477f6f9f8511f2cdadd18aacdd9f76179c
Christopher J. White <chris@grierwhite.com>
2013-06-07 16:49:00 -0400

    Open Whois links in a new window

--- config/reports/helpers/whois.py
+++ config/reports/helpers/whois.py
@@ -1,7 +1,9 @@
+def make_whois_link(ip):
+    return '<a href="http://whois.arin.net/rest/nets;q=%s?showDetails=true&showARIN=false&ext=netref2" target="_blank">Whois record</a>' % ip
+
 def whois(dst, src):
-    print "---------------In whois"
+    '''Return a data frame that simply adds a whois link for each IP'''
     df = src['t']
-    df['whois'] = df['host_ip'].map(lambda x: '<a href="http://whois.arin.net/rest/nets;q=%s?showDetails=true&showARIN=false&ext=netref2">Whois record</a>' % x)
-    print df[:10]
+    df['whois'] = df['host_ip'].map(make_whois_link)
     return df
 
