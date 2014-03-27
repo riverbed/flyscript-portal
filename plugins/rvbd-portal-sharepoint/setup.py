50db21aa99449588a9be7c2588c2a03348209438
Mike Garabedian <mike.garabedian@riverbed.com>
2014-03-27 16:41:17 -0400

    Expand sharepoint requirements for sharepoint plugin

--- plugins/rvbd-portal-sharepoint/setup.py
+++ plugins/rvbd-portal-sharepoint/setup.py
@@ -13,7 +13,7 @@ tests_require = []
 
 install_requires = [
     'Django>=1.5.1,<1.6',
-    'sharepoint==0.3.2',
+    'sharepoint>=0.3.2,<=0.4',
     'python-ntlm==1.0.1',
     'lxml>=3.3.0,<3.4.0',
     # flyscript-portal should be here too
